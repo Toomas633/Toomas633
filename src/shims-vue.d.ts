edaa15fc0ccd05bd24caea00173cbcd3e4793e54
Toomas <toomas.kirsing@gmail.com>
2024-08-03 17:03:57 +0300

    New T6Drone page

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -4,3 +4,18 @@ declare module '*.vue' {
 	const component: DefineComponent<{}, {}, any>
 	export default component
 }
+
+declare module '*.jpg' {
+	const content: string
+	export default content
+}
+
+declare module '*.png' {
+	const content: string
+	export default content
+}
+
+declare module '*.jpeg' {
+	const content: string
+	export default content
+}
--- src/views/projects/T6DroneView.vue
+++ src/views/projects/T6DroneView.vue
@@ -1,6 +1,84 @@
 <template>
 	<v-container class="pa-4">
 		<h1 class="text-center">T6 Drone</h1>
-		<p class="text-center">...</p>
+		<p class="text-center">
+			This is my first try at building a drone. It started as a schools
+			practical work project for graduation. It is built around DJI F450 frame.
+			All the 3D files, including project files and printing files, are found on
+			the <a href="https://github.com/Toomas633/T6-Drone"> GitHub page</a> . 3D
+			printing files in the STL form can be found on
+			<a href="https://www.thingiverse.com/thing:4804605">Thingiverse</a> .
+		</p>
+		<v-row class="pa-4 d-flex" justify="center">
+			<v-col>
+				<ButtonCard
+					size="100"
+					text="Download ZIP"
+					href="https://github.com/Toomas633/T6-Drone/archive/v2.zip"
+					icon="mdi-file-download-outline" />
+			</v-col>
+			<v-col>
+				<ButtonCard
+					size="100"
+					text="GitHub"
+					href="https://github.com/Toomas633/T6-Drone"
+					icon="mdi-github" />
+			</v-col>
+			<v-col>
+				<ButtonCard
+					size="100"
+					text="Thingiverse"
+					href="https://www.thingiverse.com/thing:4804605"
+					icon="mdi-rocket-launch"
+					iconColor="blue" />
+			</v-col>
+		</v-row>
+		<v-carousel
+			show-arrows="hover"
+			cycle
+			hide-delimiters
+			progress="primary"
+			interval="5000">
+			<v-carousel-item
+				v-for="(item, index) in items"
+				:key="index"
+				:alt="item.alt">
+				<v-img :src="item.src" cover>
+					<h1 class="image-title">
+						{{ item.title }}
+					</h1>
+				</v-img>
+			</v-carousel-item>
+		</v-carousel>
 	</v-container>
 </template>
+<script setup lang="ts">
+import ButtonCard from '@/components/ButtonCard.vue'
+import render from '@/assets/T6Drone/render.jpg'
+import electronics from '@/assets/T6Drone/electronics.jpg'
+import model from '@/assets/T6Drone/model.jpg'
+import complete from '@/assets/T6Drone/complete.jpg'
+
+const items = [
+	{
+		src: render,
+		title: 'KeyShot render',
+		alt: 'KeyShot render',
+	},
+	{
+		src: electronics,
+		title: 'Electronics',
+		alt: 'Electronics',
+	},
+	{
+		src: model,
+		title: 'SolidEdge model',
+		alt: 'SolidEdge model',
+	},
+	{
+		src: complete,
+		title: 'Complete build',
+		alt: 'Complete build',
+	},
+]
+</script>
